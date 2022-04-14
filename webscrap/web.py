6a25d6adcf4f06ebc14bbd4d7c6f5691627d8da1
Channabasav Angadi <channabasavangadi420@gmail.com>
2022-04-14 14:03:35 +0530

    sdf
    sdf

--- webscrap/web.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-html = requests.get('https://courses.ineuron.ai/category/Data-Science').text
-soup = BeautifulSoup(html,'lxml')
-for i in soup.find_all('div', class_='Course_flex__3ZrIo flex'):
-    anchor = i.find('h5')
-    print(anchor.text)
